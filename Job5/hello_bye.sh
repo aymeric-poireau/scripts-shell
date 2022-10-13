6708ea1364729705fe9142ca60bdead1bb2e4b85
poireau <aymeric.poireau@laplateforme.io>
2022-10-13 15:35:18 +0200

    voici les jobs

---
+++ Job5/hello_bye.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+Hello="Bonjour"
+Bye="Aurevoir"
+echo $1
+echo $2
---
+++ Job7/myloop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+i=0
+
+while [ $i -le 10 ]
+do
+  echo Je suis un script qui arrive à faire une boucle: $i
+  ((i++))
+done
